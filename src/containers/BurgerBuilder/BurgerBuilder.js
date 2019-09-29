77ca20d48c444af6d32c3779628c42c51c6fc49e
enzoesper <enzoesper@hotmail.com>
2019-09-29 16:50:46 -0300

    Adding the Burger component to display it

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxiliar from "../../hof/Auxiliar";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
   render() {
     return (
       <Auxiliar>
-        <div>Burger</div>
+        <Burger />
         <div>Build controls</div>
       </Auxiliar>
     );
