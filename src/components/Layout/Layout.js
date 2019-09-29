160f254a5e010b777e4b3d9f34a319dbf21984ad
enzoesper <enzoesper@hotmail.com>
2019-09-29 03:22:16 -0300

    Adding some initial margin between the components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxiliar from "../../hof/Auxiliar";
+import styles from "./Layout.module.css";
 
 const Layout = props => {
   return (
     <Auxiliar>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={styles.Content}>{props.children}</main>
     </Auxiliar>
   );
 };
