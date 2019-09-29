1c4c79e7e0bd7dab8be5428783cd185f3b89eba3
enzoesper <enzoesper@hotmail.com>
2019-09-29 16:30:42 -0300

    Adding proptypes package and validating it in the BurgerIngredient component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,33 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import styles from "./BurgerIngredient.module.css";
 
-const BurgerIngredient = props => {
-  let ingredient = null;
+class BurgerIngredient extends React.Component {
+  render() {
+    let ingredient = null;
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingredient = <div className={styles.BreadBottom}></div>;
-      break;
-    case "bread-top":
-      ingredient = (
-        <div className={styles.BreadTop}>
-          <div className={styles.Seeds1}></div>
-          <div className={styles.Seeds2}></div>
-        </div>
-      );
-    case "meat":
-      ingredient = <div className={styles.Meet}></div>;
-    case "cheese":
-      ingredient = <div className={styles.Cheese}></div>;
-    case "bacon":
-      ingredient = <div className={styles.Bacon}></div>;
-    case "salad":
-      ingredient = <div className={styles.Salad}></div>;
-    default:
-      ingredient = null;
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={styles.BreadBottom}></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={styles.BreadTop}>
+            <div className={styles.Seeds1}></div>
+            <div className={styles.Seeds2}></div>
+          </div>
+        );
+      case "meat":
+        ingredient = <div className={styles.Meet}></div>;
+      case "cheese":
+        ingredient = <div className={styles.Cheese}></div>;
+      case "bacon":
+        ingredient = <div className={styles.Bacon}></div>;
+      case "salad":
+        ingredient = <div className={styles.Salad}></div>;
+      default:
+        ingredient = null;
+    }
+
+    return ingredient;
   }
+}
 
-  return ingredient;
+// Adding type validation for the component, so if we ever try to
+// use the ingredient component without passing a type we will get an error
+BurgerIngredient.prototypes = {
+  type: PropTypes.string.isRequired
 };
 
 export default BurgerIngredient;
